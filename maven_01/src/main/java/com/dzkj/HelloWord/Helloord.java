684bcb055d948b00c7190195af0ac56bfae206c1
y190614 <y190614@126.com>
2019-08-19 14:12:11 +0800

    mm

--- maven_01/src/main/java/com/dzkj/HelloWord/Helloord.java
+++ maven_01/src/main/java/com/dzkj/HelloWord/Helloord.java
@@ -2,4 +2,9 @@ package com.dzkj.HelloWord;
 
 public class Helloord {
 
+	public String print() {
+		// TODO Auto-generated method stub
+		return "Helloord";
+	}
+
 }
---
+++ maven_01/src/test/java/com/dzkj/test/JTest.java
@@ -0,0 +1,19 @@
+package com.dzkj.test;
+
+
+
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+import com.dzkj.HelloWord.Helloord;
+
+public class JTest {
+	@Test
+	public void testHW(){
+		Logger log=Logger.getLogger(JTest.class);
+		log.info(new Helloord().print());
+	}
+	
+
+}
